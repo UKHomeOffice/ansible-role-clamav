c966287a81951eaf63f56c7bdd6b8926bf8f0274
Sam Frier <samfrier@gmail.com>
2016-12-06 12:09:35 +0000

    Fixed incorrect grep in freshclam_status.sh

--- files/freshclam_status.sh
+++ files/freshclam_status.sh
@@ -3,11 +3,11 @@ set -exu -o pipefail
 
 DATE=$(date '+%a %b %e')
 
-if [[ `cat /var/log/clamav/freshclam.log|grep daily.cld | tail -n1000` =~ 'up to date' ]]
+if [[ `cat /var/log/clamav/freshclam.log|grep daily | tail -n1000` =~ 'up to date' ]]
 then
   echo 'OK: Freshclam Current'
   exit 0
-elif [[ `cat /var/log/clamav/freshclam.log|grep daily.cld | tail -n1000` =~ 'updated' ]]
+elif [[ `cat /var/log/clamav/freshclam.log|grep daily | tail -n1000` =~ 'updated' ]]
 then
   echo 'OK: Freshclam Current'
   exit 0
